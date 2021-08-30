d3af7ac6ee39a6be5337a6521ff78048b36d5c28
magnuen2k <maen030@student.kristiania.no>
2021-08-30 10:10:12 +0200

    add @ApiModelProperty to all DTOs

--- cards-dto/src/main/kotlin/no/kristiania/cardgame/CardDto.kt
+++ cards-dto/src/main/kotlin/no/kristiania/cardgame/CardDto.kt
@@ -1,8 +1,19 @@
 package no.kristiania.cardgame
 
+import io.swagger.annotations.ApiModelProperty
+
 class CardDto (
+        @get:ApiModelProperty("")
         var cardId: String? = null,
+
+        @get:ApiModelProperty()
         var name: String? = null,
+
+        @get:ApiModelProperty("")
         var description: String? = null,
+
+        @get:ApiModelProperty("")
         var rarity: Rarity? = null,
+
+        @get:ApiModelProperty("")
         var imageId: String? = null)
--- cards-dto/src/main/kotlin/no/kristiania/cardgame/CollectionDto.kt
+++ cards-dto/src/main/kotlin/no/kristiania/cardgame/CollectionDto.kt
@@ -1,12 +1,18 @@
 package no.kristiania.cardgame
 
+import io.swagger.annotations.ApiModelProperty
+
 class CollectionDto (
 
+        @get:ApiModelProperty("")
         var cards: MutableList<CardDto> = mutableListOf(),
 
+        @get:ApiModelProperty("")
         var prices: MutableMap<Rarity, Int> = mutableMapOf(),
 
+        @get:ApiModelProperty("")
         var millValues: MutableMap<Rarity, Int> = mutableMapOf(),
 
+        @get:ApiModelProperty("")
         var rarityProbabilities: MutableMap<Rarity, Double> = mutableMapOf()
 )
