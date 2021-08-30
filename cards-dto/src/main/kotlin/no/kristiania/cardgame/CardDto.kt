1f5d103dd878f4b9e8aafc145310805c7acc9c0d
magnuen2k <maen030@student.kristiania.no>
2021-08-30 15:34:28 +0200

    adding swagger documentation to card/collection Dto's

--- cards-dto/src/main/kotlin/no/kristiania/cardgame/CardDto.kt
+++ cards-dto/src/main/kotlin/no/kristiania/cardgame/CardDto.kt
@@ -3,17 +3,17 @@ package no.kristiania.cardgame
 import io.swagger.annotations.ApiModelProperty
 
 class CardDto (
-        @get:ApiModelProperty("")
+        @get:ApiModelProperty("Card Id")
         var cardId: String? = null,
 
-        @get:ApiModelProperty()
+        @get:ApiModelProperty("Name of the card")
         var name: String? = null,
 
-        @get:ApiModelProperty("")
+        @get:ApiModelProperty("Description of the card")
         var description: String? = null,
 
-        @get:ApiModelProperty("")
+        @get:ApiModelProperty("Rarity of the card")
         var rarity: Rarity? = null,
 
-        @get:ApiModelProperty("")
+        @get:ApiModelProperty("Id of image to card")
         var imageId: String? = null)
--- cards-dto/src/main/kotlin/no/kristiania/cardgame/CollectionDto.kt
+++ cards-dto/src/main/kotlin/no/kristiania/cardgame/CollectionDto.kt
@@ -4,15 +4,15 @@ import io.swagger.annotations.ApiModelProperty
 
 class CollectionDto (
 
-        @get:ApiModelProperty("")
+        @get:ApiModelProperty("List of cards in the collection")
         var cards: MutableList<CardDto> = mutableListOf(),
 
-        @get:ApiModelProperty("")
+        @get:ApiModelProperty("Prices of the different rarities")
         var prices: MutableMap<Rarity, Int> = mutableMapOf(),
 
-        @get:ApiModelProperty("")
+        @get:ApiModelProperty("Values for milling different rarities")
         var millValues: MutableMap<Rarity, Int> = mutableMapOf(),
 
-        @get:ApiModelProperty("")
+        @get:ApiModelProperty("Probability for getting the different rarities")
         var rarityProbabilities: MutableMap<Rarity, Double> = mutableMapOf()
 )
