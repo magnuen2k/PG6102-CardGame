fdcaec299fb3fe1c6cb8a554003b8e815a13215a
magnuen2k <maen030@student.kristiania.no>
2021-09-13 10:25:44 +0200

    added scores and rest-dto modules

---
+++ scores/src/main/kotlin/no/kristiania/Hello.kt
@@ -0,0 +1,6 @@
+package no.kristiania
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ scores/src/test/kotlin/no/kristiania/HelloTest.kt
@@ -0,0 +1,8 @@
+package no.kristiania
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
